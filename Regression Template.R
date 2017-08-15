8bb2aa82b16104aa2261e932f72a639a6d605ed0
Regis <X-lem@hotmail.com>
2017-08-15 16:40:49 -0600

    Update R Regression Template

--- Regression Template.R
+++ Regression Template.R
@@ -1,16 +1,16 @@
 # Regression Template
 
 # Importing the dataset
-dataset = read.csv('Position_Salaries.csv')
-dataset = dataset[2:3]
+ds = read.csv('Position_Salaries.csv')
+ds = ds[2:3]
 
 # Splitting the dataset into the Training set and Test set
 # # install.packages('caTools')
 # library(caTools)
 # set.seed(123)
-# split = sample.split(dataset$Salary, SplitRatio = 2/3)
-# training_set = subset(dataset, split == TRUE)
-# test_set = subset(dataset, split == FALSE)
+# split = sample.split(ds$Salary, SplitRatio = 2/3)
+# training_set = subset(ds, split == TRUE)
+# test_set = subset(ds, split == FALSE)
 
 # Feature Scaling
 # training_set = scale(training_set)
@@ -26,9 +26,9 @@ y_pred = predict(regressor, data.frame(Level = 6.5))
 # install.packages('ggplot2')
 library(ggplot2)
 ggplot() +
-  geom_point(aes(x = dataset$Level, y = dataset$Salary),
+  geom_point(aes(x = ds$Level, y = ds$Salary),
              colour = 'red') +
-  geom_line(aes(x = dataset$Level, y = predict(regressor, newdata = dataset)),
+  geom_line(aes(x = ds$Level, y = predict(regressor, newdata = ds)),
             colour = 'blue') +
   ggtitle('Truth or Bluff (Regression Model)') +
   xlab('Level') +
@@ -37,9 +37,9 @@ ggplot() +
 # Visualising the Regression Model results (for higher resolution and smoother curve)
 # install.packages('ggplot2')
 library(ggplot2)
-x_grid = seq(min(dataset$Level), max(dataset$Level), 0.1)
+x_grid = seq(min(ds$Level), max(ds$Level), 0.1)
 ggplot() +
-  geom_point(aes(x = dataset$Level, y = dataset$Salary),
+  geom_point(aes(x = ds$Level, y = ds$Salary),
              colour = 'red') +
   geom_line(aes(x = x_grid, y = predict(regressor, newdata = data.frame(Level = x_grid))),
             colour = 'blue') +
