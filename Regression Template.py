644294c585371a4e091ddcd009274351ca371a3e
Regis <X-lem@hotmail.com>
2017-08-16 14:42:56 -0600

    Update Regression Template Python

--- Regression Template.py
+++ Regression Template.py
@@ -12,7 +12,7 @@ Y = ds.iloc[:, 2].values
 
 # Splitting the dataset into the Training set and Test set
 """from sklearn.cross_validation import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size = 0.2, random_state = 0)"""
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.2, random_state = 0)"""
 
 # Feature Scaling
 """from sklearn.preprocessing import StandardScaler
@@ -20,7 +20,7 @@ sc_X = StandardScaler()
 X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)
 sc_y = StandardScaler()
-y_train = sc_y.fit_transform(y_train)"""
+Y_train = sc_y.fit_transform(Y_train)"""
 
 # Fitting the Regression Model to the dataset
 # Create your regressor here
