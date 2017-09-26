49ade74128313c7f6ce10f6816f8903cf8a5ffe9
Regis <X-lem@hotmail.com>
2017-09-26 16:35:45 -0600

    Natural Language Processing - Python

--- Natural Language Processing.py
+++ Natural Language Processing.py
@@ -31,3 +31,67 @@ for i in range(0, 1000):
 from sklearn.feature_extraction.text import CountVectorizer
 cv = CountVectorizer(max_features = 1500)
 X = cv.fit_transform(corpus).toarray()
+Y = ds.iloc[:, 1].values
+
+####### Naive Bayes
+
+# Splitting the dataset into a training set and test set
+from sklearn.cross_validation import train_test_split
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.20, random_state = 0)
+
+
+# Fitting Naive Bayes to the Training Set
+from sklearn.naive_bayes import GaussianNB
+cl = GaussianNB()
+cl.fit(X_train, Y_train)
+
+# Predicint the test set results
+y_pred = cl.predict(X_test)
+
+# Making the confustion matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(Y_test, y_pred)
+
+
+####### End Naive Bayes
+
+####### Decision Tree Classification
+
+# Splitting the dataset into a training set and test set
+from sklearn.cross_validation import train_test_split
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.20, random_state = 0)
+
+
+# Fitting Decision Tree Classification to the Training Set
+from sklearn.tree import DecisionTreeClassifier
+cl = DecisionTreeClassifier(criterion = 'entropy', random_state = 0)
+cl.fit(X_train, Y_train)
+
+# Predicint the test set results
+y_pred = cl.predict(X_test)
+
+# Making the confustion matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(Y_test, y_pred)
+
+####### End Decision Tree Classification
+
+####### Random Forest Classification
+# Splitting the dataset into a training set and test set
+from sklearn.cross_validation import train_test_split
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.20, random_state = 0)
+
+
+# Fitting the Random Forest classifier to the Training Set
+from sklearn.ensemble import RandomForestClassifier
+cl = RandomForestClassifier(n_estimators = 10, criterion = 'entropy', random_state = 0)
+cl.fit(X_train, Y_train)
+
+# Predicint the test set results
+y_pred = cl.predict(X_test)
+
+# Making the confustion matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(Y_test, y_pred)
+
+####### End Random Forest Classification
