13013059fbc21fde7fa20b383090d04178555e33
Regis <X-lem@hotmail.com>
2017-10-14 13:34:20 -0600

    Principal Component Analysis - Python

---
+++ Principal Component Analysis.py
@@ -0,0 +1,80 @@
+# Principal Component Analysis (PCA)
+
+
+# Import the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+ds = pd.read_csv("Wine.csv")
+X = ds.iloc[:, 0:13].values
+Y = ds.iloc[:, 13].values
+
+# Splitting the dataset into a training set and test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.20, random_state = 0)
+
+# Feature scaling
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+
+# Apply dimensionality reduction (PCA)
+from sklearn.decomposition import PCA
+pca = PCA(n_components = 2) # Extract the top 2 principle components
+X_train = pca.fit_transform(X_train)
+X_test = pca.transform(X_test)
+# Explained Variance
+exv = pca.explained_variance_ratio_
+
+
+# Fitting the PCA to the Training Set (logistic regression)
+from sklearn.linear_model import LogisticRegression
+cl = LogisticRegression(random_state = 0)
+cl.fit(X_train, Y_train)
+
+
+# Predicint the test set results
+y_pred = cl.predict(X_test)
+
+# Making the confustion matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(Y_test, y_pred)
+
+# Visualising the Training set results
+from matplotlib.colors import ListedColormap
+X_set, Y_set = X_train, Y_train
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, cl.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('yellow', 'green', 'blue')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(Y_set)):
+    plt.scatter(X_set[Y_set == j, 0], X_set[Y_set == j, 1],
+                c = ListedColormap(('purple', 'cyan', 'orange'))(i), label = j)
+plt.title('Principal Component Analysis (Training set)')
+plt.xlabel('PC1')
+plt.ylabel('PC2')
+plt.legend()
+plt.show()
+
+# Visualising the Test set results
+from matplotlib.colors import ListedColormap
+X_set, Y_set = X_test, Y_test
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, cl.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('yellow', 'green', 'blue')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(Y_set)):
+    plt.scatter(X_set[Y_set == j, 0], X_set[Y_set == j, 1],
+                c = ListedColormap(('purple', 'cyan', 'orange'))(i), label = j)
+plt.title('Principal Component Analysis (Test set)')
+plt.xlabel('PC1')
+plt.ylabel('PC2')
+plt.legend()
+plt.show()
